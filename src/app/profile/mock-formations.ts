a7023b8d895a0413e9727cc305dbfc1adcee6544
Mihaela Bucatariu <mbucatariu@outloook.fr>
2020-10-15 07:56:34 +0200

    Update Udemy training

--- src/app/profile/mock-formations.ts
+++ src/app/profile/mock-formations.ts
@@ -8,6 +8,33 @@ export const FORMATIONS: Formation[] = [
     `Diplômé d'ingénieur ordinateurs, spécialisation Systèmes de programmes (logiciel), Université
     Polytechnique de Bucarest, la Faculté d'Automatique et Ordinateurs`
   },
+  { diplome: "The Complete Web Development Bootcamp",
+  period: "Avril 2020 à décembre 2020 ",
+  ecole: "Udemy",
+  description:
+  `Compétences acquises : Développement Web, Javascript, React, HTML, CSS, Bootstrap, NodeJs, MongoDB
+  Environnement : Visual Studio Code, Microsoft Edge, Microsoft Windows`
+  },
+  { diplome: "Complete Python Developer in 2020",
+  period: "Avril 2020 à décembre 2020 ",
+  ecole: "Zero to Mastery Academy (Udemy)",
+  description:
+  `Compétences acquises : Python, Développement Web avec Flask, Web Scraping
+  Environnement : PyCharm, Jupyter Notebooks, Microsoft Windows`
+  },
+  { diplome: "Python and Django Full Stack Web Developer Bootcamp",
+  period: "Avril 2020 à décembre 2020 ",
+  ecole: "Udemy",
+  description:
+  `Compétences acquises : Python, Développement Web avec Django, Javascript, HTML, CSS, Bootstrap
+  Environnement : Visual Studio Code, PyCharm, Microsoft Windows`
+  },
+  { diplome: "Introduction to Cloud Computing on Amazon AWS for Beginners",
+  period: "Avril 2020 à juin 2020 ",
+  ecole: "Udemy",
+  description:
+  `Compétences acquises : les bases de Amazon AWS, les concepts du cloud computing `
+  },
   { diplome: "Développeur C++ Linux embarqué",
   period: "Mars 2018 à juin 2018 ",
   ecole: "M2iFormation",
